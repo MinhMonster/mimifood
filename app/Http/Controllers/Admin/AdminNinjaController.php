357628df99beedc1c21d32df5ee2f39cdf010155
minhdc <thainv@hexigon.tech>
2026-02-11 05:10:50 +0700

    refactor admin game account

--- app/Http/Controllers/Admin/AdminNinjaController.php
+++ app/Http/Controllers/Admin/AdminNinjaController.php
@@ -15,32 +15,40 @@ class AdminNinjaController extends AdminGameAccountController
 
     protected function rules(?int $id = null): array
     {
-        return [
-            'code' => [
-                'nullable',
-                'integer',
-                'min:1',
-                Rule::unique('ninjas', 'code')->ignore($id),
+        return array_merge(
+            [
+                'code' => [
+                    'nullable',
+                    'integer',
+                    'min:1',
+                    Rule::unique('ninjas', 'code')->ignore($id),
+                ],
+                'username' => [
+                    'required',
+                    'string',
+                    Rule::unique('ninjas')
+                        ->ignore($id)
+                        ->whereNull('deleted_at'),
+                ],
+                'character_name' => 'required|string',
+                'description' => 'required|string',
+                'images' => 'required|array',
+                'is_full_image' => 'required|boolean',
+                'selling_price' => 'required|integer',
+                'purchase_price' => 'required|integer',
+                'discount_percent' => 'nullable|integer|max:50',
+                'class' => 'required|integer',
+                'level' => 'required|integer|max:160',
+                'server' => 'required|integer',
+                'weapon' => 'required|integer|max:16',
+                'type' => ['required', Rule::in(['1', '2', '3'])],
             ],
-            'username' => [
-                'required',
-                'string',
-                Rule::unique('ninjas')->ignore($id)->whereNull('deleted_at'),
-            ],
-            'character_name' => 'required|string',
-            'description' => 'required|string',
-            'images' => 'required|array',
-            'is_full_image' => 'required|boolean',
-            'selling_price' => 'required|integer',
-            'purchase_price' => 'required|integer',
-            'discount_percent' => 'nullable|integer|max:50',
-            'class' => 'required|integer',
-            'level' => 'required|integer|max:160',
-            'server' => 'required|integer',
-            'weapon' => 'required|integer|max:16',
-            'type' => ['required', Rule::in(['1','2','3'])],
-            ...collect(range(1, 12))->mapWithKeys(fn ($i) => ["tl_$i" => 'nullable|integer|max:9'])->all(),
-            ...collect(range(1, 13))->mapWithKeys(fn ($i) => ["item_$i" => 'nullable|string'])->all(),
-        ];
+            collect(range(1, 12))
+                ->mapWithKeys(fn($i) => ["tl_$i" => 'nullable|integer|max:9'])
+                ->all(),
+            collect(range(1, 13))
+                ->mapWithKeys(fn($i) => ["item_$i" => 'nullable|string'])
+                ->all(),
+        );
     }
 }
